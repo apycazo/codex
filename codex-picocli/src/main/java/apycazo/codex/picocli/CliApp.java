e0fd20afe4cb9f266bafe63a37f1f30079809a2f
andres picazo <apycazo@gmail.com>
2020-08-26 22:11:56 +0200

    Removed commented code

--- codex-picocli/src/main/java/apycazo/codex/picocli/CliApp.java
+++ codex-picocli/src/main/java/apycazo/codex/picocli/CliApp.java
@@ -5,7 +5,6 @@ import picocli.CommandLine;
 public class CliApp {
 
   public static void main(String... args) {
-//    new CommandLine(new TopCommand()).usage(System.out);
     new CommandLine(new TopCommand()).execute(args);
   }
 }
