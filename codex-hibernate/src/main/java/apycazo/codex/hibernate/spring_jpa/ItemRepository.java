272e263f2e9c894c82f9e0afdf51eb45b51cbc8a
Andres Picazo <apycazo@gmail.com>
2020-12-10 19:35:21 +0100

    codex-hibernate: added HQL examples and fixed dependencies.

--- codex-hibernate/src/main/java/apycazo/codex/hibernate/spring_jpa/ItemRepository.java
+++ codex-hibernate/src/main/java/apycazo/codex/hibernate/spring_jpa/ItemRepository.java
@@ -1,6 +1,18 @@
 package apycazo.codex.hibernate.spring_jpa;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+// see reference: https://docs.spring.io/spring-data/jpa/docs/2.4.1/reference/html/#reference
 public interface ItemRepository extends CrudRepository<ItemEntity, Integer> {
+
+  @Query("select i from ItemEntity i where i.value <= ?1")
+  List<ItemEntity> findItemsWithValueLessThan(int value);
+
+  @Query("select i from ItemEntity i where i.value = ?1")
+  ItemEntity findItemWithValue(int value);
+
+  int countByValueLessThan(int value);
 }
--- codex-hibernate/src/main/java/apycazo/codex/hibernate/spring_jpa/SpringJpaDemoService.java
+++ codex-hibernate/src/main/java/apycazo/codex/hibernate/spring_jpa/SpringJpaDemoService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -18,13 +19,30 @@ public class SpringJpaDemoService {
     item.setName("A");
     item.setValue(10);
     ItemEntity savedItem = itemRepository.save(item);
+    int id = savedItem.getId();
     log.info("Saved item: {}", savedItem);
     // fetch from database
-    Optional<ItemEntity> maybeItem = itemRepository.findById(savedItem.getId());
+    Optional<ItemEntity> maybeItem = itemRepository.findById(id);
     if (maybeItem.isEmpty()) {
-      log.warn("Item not found with ID: {}", savedItem.getId());
+      log.warn("Item not found with ID: {}", id);
     } else {
       log.info("Read item: {}", maybeItem.get());
+      itemRepository.deleteById(id);
     }
+
+    for (int i = 0; i < 10; i++) {
+      ItemEntity newItem = new ItemEntity();
+      newItem.setName("item-" + i);
+      newItem.setValue(10*i);
+      log.info("Saved value: {}", itemRepository.save(newItem));
+    }
+
+    List<ItemEntity> itemsWithValueLessThan45 = itemRepository.findItemsWithValueLessThan(45);
+    log.info("Items with value <= 45: {}", itemsWithValueLessThan45);
+
+    ItemEntity item40 = itemRepository.findItemWithValue(40);
+    log.info("Item 40: {}", item40);
+
+    log.info("Count of items with value less than 30: {}", itemRepository.countByValueLessThan(30));
   }
 }
